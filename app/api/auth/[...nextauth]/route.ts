392c1fff38b87371a17082975c79a5b12c8c6d7c
chocosykr <sejalrju8@gmail.com>
2026-03-28 16:54:44 +0530

    tried to fix next auth issues 2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
+console.log('[nextauth] NEXTAUTH_URL:', process.env.NEXTAUTH_URL);
+
 process.env.NEXTAUTH_URL = process.env.NEXTAUTH_URL ?? "https://campus-oss.vercel.app";
 
 const handler = NextAuth(authOptions);
